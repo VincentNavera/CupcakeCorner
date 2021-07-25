ee235c0c0592f2625321ab5447c8ae7a7791e841
vincio <airfoil_elector0b@icloud.com>
2021-07-25 22:12:16 +0800

    added form sections

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var order = Order()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
